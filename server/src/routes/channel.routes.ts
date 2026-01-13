3cde575f0d61205f87ec1975f4935bf09e2003fc
kapsi1 <kapsiakk@gmail.com>
2026-01-13 15:23:44 +0100

    Fix problem with messages not appearing on reload

--- server/src/routes/channel.routes.ts
+++ server/src/routes/channel.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { createChannel, getChannels } from "../controllers/channel.controller";
+import { getChannelMessages } from "../controllers/message.controller";
 import { authenticate } from "../middleware/auth.middleware";
 
 const router = Router();
 
 router.get("/", authenticate, getChannels);
 router.post("/", authenticate, createChannel);
+router.get("/:channelId/messages", authenticate, getChannelMessages);
 
 export default router;
--- server/src/routes/message.routes.ts
+++ server/src/routes/message.routes.ts
@@ -1,13 +1,11 @@
 import { Router } from "express";
 import {
 	createMessage,
-	getChannelMessages,
 } from "../controllers/message.controller";
 import { authenticate } from "../middleware/auth.middleware";
 
 const router = Router();
 
-router.get("/:channelId/messages", authenticate, getChannelMessages);
 router.post("/", authenticate, createMessage);
 
 export default router;
