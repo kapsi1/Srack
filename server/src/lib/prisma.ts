693398bb3ef87282d35c3284298f80b94401983b
kapsi1 <kapsiakk@gmail.com>
2026-01-13 18:10:01 +0100

    Update Prisma 6 -> 7

--- server/src/lib/prisma.ts
+++ server/src/lib/prisma.ts
@@ -1,5 +1,14 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
+import dotenv from "dotenv";
 
-const prisma = new PrismaClient();
+dotenv.config();
+
+const connectionString = process.env.DATABASE_URL?.replace("localhost", "127.0.0.1");
+const pool = new Pool({ connectionString });
+
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });
 
 export default prisma;
